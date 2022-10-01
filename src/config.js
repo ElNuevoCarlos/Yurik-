2c26e7264fce315b6132305e718d27a058207fd1
Carlos <69126001+ElNuevoCarlos@users.noreply.github.com>
2022-10-01 10:43:24 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   id: "1025431264901202000",
-  clientSecret: "R74prsAsVzJHOM87QLbUhGDo1gCHMdY1",
-  token: "MTAyNTQzMTI2NDkwMTIwMjAwMA.G18KcB.Ny9Gry7V-GmozverCLsFiFa6oXVbTtUoOZ1xC8",
+  clientSecret: "....",
+  token: ".....",
   port: 3000,
   customDomain: false,
   domain: "http://localhost:3000"
